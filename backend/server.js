45039cc63cf63623aadf3e54224198fa1aa7f8a7
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-19 12:13:17 +0300

    Testing CRUD
    * Done testing all CRUD methods on bruno
    * Moving to create routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,82 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/products", (req, res) => {
-  res.send("Hello World");
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error fetching products", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res
+      .status(201)
+      .json({ success: true, message: "Product created successfully" });
+  } catch (error) {
+    console.log("Error creating product", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    console.log("Error updating product", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting product", error.message);
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
 });
 
 app.listen(5000, () => {
-    connectDB();
+  connectDB();
   console.log("Server is running on port 5000");
 });
-
-
-
