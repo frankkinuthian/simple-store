93adcff951c472b9ad07e5c63c6ddc72f6ef839e
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-19 13:55:24 +0300

    Navbar complete
    * Simple Navbar component with three buttons
    * Added toggle to switch color modes to main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+import { useColorModeValue } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
