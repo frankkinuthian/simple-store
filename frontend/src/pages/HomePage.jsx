f8a88b5ed8e4c0d7fc4fb454ed5af0d4eeebf10f
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-19 17:28:17 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,42 @@
-
+import { Container, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW={"container.xl"} py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products
+        </Text>
 
-export default HomePage
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight={"bold"}
+          color="gray.500"
+        >
+          No Products Found 🥲{""}
+          <Link to={"/create"}>
+            <Text
+              as={"span"}
+              color="blue.500"
+              _hover={{
+                textDecoration: "underline",
+              }}
+            >
+              Create Product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomePage;
